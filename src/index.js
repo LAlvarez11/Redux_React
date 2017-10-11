985e0c691ece168442902adf9f54d0dd2470b163
LAlvarez11 <alvarezluis121@gmail.com>
2017-10-10 23:03:27 -0700

    Added the Thumbnail and video title to the list on the web app

--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom';
 import {SearchBar} from './components/searchBar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/videoList';
-
 const API_KEY = 'AIzaSyBo7vlfKToMVqI9uaqtqtsdS4SQtfzQnVA';
 
 
-class App extends Component{
+class App extends Component {
   constructor(props){
     super(props);
-    this.state = { videos: [] };
 
-    YTSearch({key: API_KEY, term:'surfboards'},(videos) => {
-      this.setState=( { videos });
+    this.state = { videos:[] };
+
+    YTSearch({key: API_KEY, term: 'basketball'},(results) =>{
+      this.setState({videos : results});
+      console.log(results);
+      console.log(this.state.videos);
     });
   }
 
